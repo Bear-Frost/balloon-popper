904d759b7b1db68ef2b0829813bba3462767b613
shaian <zshaian@users.noreply.github.com>
2024-12-20 14:30:06 +0800

    chore: add type annotation for Jest configuration

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,9 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
-
+/**
+ * @type {import('jest').Config} 
+ */
 const config = {
   clearMocks: true,
   collectCoverage: true,
