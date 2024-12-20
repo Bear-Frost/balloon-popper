6cf6c33d0bc773f3df9fb580897dbe170491e593
shaian <zshaian@users.noreply.github.com>
2024-12-20 14:28:06 +0800

    feat: add initial Jest configuration file

---
+++ jest.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+};
+
+export default config;
