a4a22177aae8474b8e1e63edf177eda5224c14e7
shaian <zshaian@users.noreply.github.com>
2024-12-22 17:27:07 +0800

    chore: bump version to 1.1.1 and update output file extensions in Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,8 @@ export default [
   {
     input: "./src/index.js",
     output: [
-      { file: "dist/index.cjs.js", format: "cjs" },
-      { file: "dist/index.esm.js", format: "es" },
+      { file: "dist/index.cjs", format: "cjs" },
+      { file: "dist/index.esm.mjs", format: "es" },
       { file: "dist/index.umd.js", format: "umd", name: "unitFlip" },
     ],
     plugins:[
