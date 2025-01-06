79b15fa6b4d7fb24fc1d91b76783fc1e2a96cccf
shaian <zshaian@users.noreply.github.com>
2025-01-06 21:45:53 +0800

    feat: add a utility function for getting the root line height unit value

---
+++ src/utils/getRootLineHeight.js
@@ -0,0 +1,26 @@
+/**
+ * Get the root line height value.
+ *
+ * @returns {number} The root line height value.
+ */
+function getRootLineHeight() {
+  const isWindowValid =
+    typeof window !== "undefined" &&
+    typeof window.document !== "undefined" &&
+    typeof window.getComputedStyle === "function";
+
+  if (!isWindowValid) return 24; // default root line height value.
+  const rootComputedStyle = window.getComputedStyle(document.documentElement);
+  let rootLineHeightValue = rootComputedStyle.lineHeight;
+
+  if (rootLineHeightValue === "normal") {
+    const rootFontSize = parseFloat(rootComputedStyle.fontSize);
+    const defaultLineHeightMultiplier = 1.5;
+
+    return rootLineHeightValue = rootFontSize * defaultLineHeightMultiplier;
+  }
+
+  return rootLineHeightValue;
+}
+
+export { getRootLineHeight };
